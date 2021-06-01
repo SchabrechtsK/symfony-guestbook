9edddfecb7985ef3bebab18a99ff8218adb7fb95
SchabrechtsK <schabrechts.k@gmail.com>
2021-06-01 15:24:54 +0200

    Add a router and update the app

--- spa/src/app.js
+++ spa/src/app.js
@@ -1,9 +1,22 @@
 import {h, render} from 'preact';
+import {Router, Link} from 'preact-router';
+
+import Home from './pages/home';
+import Conference from './pages/conference';
 
 function App() {
     return (
         <div>
-            Hello world!
+            <header>
+                <Link href="/">Home</Link>
+                <br />
+                <Link href="/conference/london-2021">London 2021</Link>
+            </header>
+
+            <Router>
+                <Home path="/" />
+                <Conference path="/conference/:slug" />
+            </Router>
         </div>
     )
 }
