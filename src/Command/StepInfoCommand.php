cc2dda32fe0e9ed88ef107be4d1bcd085c8949b9
SchabrechtsK <schabrechts.k@gmail.com>
2021-06-01 11:42:04 +0200

    Add a command to see the process package in action

---
+++ src/Command/StepInfoCommand.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Process\Process;
+
+class StepInfoCommand extends Command
+{
+    protected static $defaultName = 'app:step:info';
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+       $process = new Process(['git', 'tag', '-l', '--points-at', 'HEAD']);
+       $process->mustRun();
+       $output->write($process->getOutput());
+
+       return 0;
+    }
+}
